7eb2b276330c6a95ebefe0b893e0e714f6cc64af
Marcelopa24 <marcelopa94@hotmail.com>
2022-10-05 20:12:46 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from "react";
+import Formulario from "./componentes/Formulario";
 
 class App extends React.Component{
   render(){
     return(
-      <div class="container">
-        <h1> Hello Dojo!</h1>
-        <h1> Things I need to do: </h1>
-        <ul>
-          <li>Learn React</li>
-          <li>Learn Python</li>
-          <li>Learn Flask</li>
-          <li>Learn Jinja</li>
-          <li>Learn Node</li>
-          <li>Learn Npm</li>
-        </ul>
+      <div class="container col-3 mt-3">
+        <Formulario/>
       </div>
     )
   }
